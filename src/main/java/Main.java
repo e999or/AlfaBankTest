3a21bb51f35167b46c8d43af17ea655295a3fb68
Eg <e999or@bk.ru>
2022-06-03 18:24:35 +0300

    compare in main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,7 +10,7 @@ public class Main {
         JSONObject jsonIndex = indexReader.readJsonFromUrl("https://openexchangerates.org/api/latest.json?app_id=f2aa75f4b0f04ad9bcb2157a8e5fbd98");
         JSONObject rates = jsonIndex.getJSONObject("rates");
         String usdIndex = rates.getString("RUB");
-        System.out.println(usdIndex);
+        double usdNow = Double.parseDouble(usdIndex);
 
 
         String dateLater = Clock.date();
@@ -18,11 +18,18 @@ public class Main {
         JSONObject pastJsonIndex = pastIndexReader.readJsonFromUrl("https://openexchangerates.org/api/historical/" + dateLater + ".json?app_id=f2aa75f4b0f04ad9bcb2157a8e5fbd98");
         JSONObject pastRates = pastJsonIndex.getJSONObject("rates");
         String pastUsdIndex = pastRates.getString("RUB");
-        System.out.println(pastUsdIndex);
+        double usdPast = Double.parseDouble(pastUsdIndex);
 
+        String tag;
+
+        if (usdPast < usdNow){
+            tag = "rich";
+        }else
+            tag = "broke";
+;
 
         JsonReader GifReader = new JsonReader();
-        JSONObject json = GifReader.readJsonFromUrl("https://api.giphy.com/v1/gifs/random?api_key=9ekhBW8jmXb47DiBR65QvezW10qXlszE&tag=rich&rating=g");
+        JSONObject json = GifReader.readJsonFromUrl("https://api.giphy.com/v1/gifs/random?api_key=9ekhBW8jmXb47DiBR65QvezW10qXlszE&tag=" + tag + "&rating=g");
         JSONObject data = json.getJSONObject("data");
         String idString = data.getString("id");
         String url = "https://i.giphy.com/" + idString + ".gif";
